af4fc774dc20a8068ebd62391098bd3cc20cdbea
majikjonno <53867143+majikjonno@users.noreply.github.com>
2020-06-22 12:23:53 +0800

    Update second.sh

--- week1/second.sh
+++ week1/second.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#prints "Hi there!" to screen and on second line "It's good to see you" followed by variable given by user when running script
  echo "Hi there!"
  echo "It's good to see you $1!"
  exit 0
